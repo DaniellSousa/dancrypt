c31c38d103bffea9b14c708cbc34364219815b67
Daniel Monteiro <daniel.monteiro@deway.com.br>
2019-10-29 09:52:50 -0300

    Files for configurations

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+setup(
+  name = 'dancrypt',
+  packages = ['crypt'],
+  version = '0.1',
+  license='DANIEL MONTEIRO',
+  description = 'A Simple Encryptor and Decryptor',
+  author = 'Daniel Monteiro',
+  author_email = 'daniell.monteirosousa@gmail.com',
+  url = 'https://github.com/DaniellSousa/dancrypt',
+  download_url = 'https://github.com/DaniellSousa/dancrypt/archive/master.zip',
+  keywords = ['key', 'password', 'security', 'encrypt', 'decrypt', 'text', 'message'],
+  install_requires=[
+    ],
+  classifiers=[  # Optional
+    # # How mature is this project? Common values are
+    # #   3 - Alpha
+    # #   4 - Beta
+    # #   5 - Production/Stable
+    # 'Development Status :: 3 - Alpha',
+    #
+    # # Indicate who your project is intended for
+    # 'Intended Audience :: Developers',
+    # 'Topic :: Software Development :: Build Tools',
+    #
+    # # Pick your license as you wish
+    # 'License :: OSI Approved :: MIT License',
+    #
+    # # Specify the Python versions you support here. In particular, ensure
+    # # that you indicate whether you support Python 2, Python 3 or both.
+    # 'Programming Language :: Python :: 3',
+    # 'Programming Language :: Python :: 3.4',
+    # 'Programming Language :: Python :: 3.5',
+    # 'Programming Language :: Python :: 3.6',
+  ],
+)
